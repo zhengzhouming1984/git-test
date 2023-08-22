d68c6c1beba8b07bacd4149d25181bd85612eaf2
zhengzhouming <94711123@qq.com>
2023-08-22 16:24:06 +0800

    master commit3

--- src/main/java/com/zhengzhouming/git/GitTest.java
+++ src/main/java/com/zhengzhouming/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("mater test!");
         System.out.println("hot-fix test!");
+        System.out.println("master1 test!");
     }
 }
